30f410f0f6f54cd7bce299b39f90684d9fc7b419
Amy Lin <Amy@amys-macbook-pro-3.local>
2016-03-31 18:51:40 -0700

    Initial Commit

---
+++ Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by Amy Lin on 3/31/16.
+//  Copyright © 2016 Amy Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
